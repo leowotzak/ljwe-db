8c9a840307b1e97d2006df8a0269cfcf304efb96
Leo Wotzak <leojwotzak@gmail.com>
2021-11-12 16:55:14 -0600

    added wrapper to format output

---
+++ ljwe-db/main.py
@@ -0,0 +1,17 @@
+COL_NAMES = {
+    '1. open': 'open_price',
+    '2. high': 'high_price', 
+    '3. low': 'low_price',
+    '4. close': 'close_price',
+    '5. volume': 'volume',
+}
+
+
+
+
+def bar_data_wrapper(func):
+    """Standardizes column names for any bar data"""
+    def wrapper(*args, **kwargs):
+        res: pd.DataFrame = func(*args, **kwargs)
+        return res.rename(columns=COL_NAMES)   
+    return wrapper
