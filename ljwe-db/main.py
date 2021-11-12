b3e84234afcf827eed68c465fdcf755fe9d2ad22
Leo Wotzak <leojwotzak@gmail.com>
2021-11-12 16:57:32 -0600

    implemented 'get listed symbols'

--- ljwe-db/main.py
+++ ljwe-db/main.py
@@ -1,3 +1,7 @@
+
+import pandas as pd
+import requests
+from config import Config
 COL_NAMES = {
     '1. open': 'open_price',
     '2. high': 'high_price', 
@@ -15,3 +19,11 @@ def bar_data_wrapper(func):
         res: pd.DataFrame = func(*args, **kwargs)
         return res.rename(columns=COL_NAMES)   
     return wrapper
+
+
+def _get_listed_symbols() -> pd.DataFrame:
+    """Retrieves currently listed symbols from alphavantage"""
+    res = requests.get(Config.base_url, params=_generate_query("LISTING_STATUS"))
+    csv = str(res.content, encoding="utf-8")
+    return pd.read_csv(csv, header=0)
+
