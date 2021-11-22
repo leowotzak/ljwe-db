faf8b0b0dab3781f3a877ecdec82f229bc357985
Leo Wotzak <leojwotzak@gmail.com>
2021-11-21 18:47:37 -0600

    added restful_framework

--- ljwe/api/urls.py
+++ ljwe/api/urls.py
@@ -1,6 +1,24 @@
-from django.urls import path
-from . import views
+
+
+from django.urls import path, include
+from .models import Symbol
+from rest_framework import routers, serializers, viewsets
+
+class QuerySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Symbol
+        fields = ['symbol_id', 'ticker']
+
+class SymbolViewSet(viewsets.ModelViewSet):
+    queryset = Symbol.objects.all()
+    serializer_class = QuerySerializer
+
+router = routers.DefaultRouter()
+router.register(r'symbol', SymbolViewSet)
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls'))
 ]
+
+
