c77acd5a4ca69a60196b0e010207fe3208580031
Leo Wotzak <leojwotzak@gmail.com>
2021-11-22 13:38:51 -0600

    tweaked settings

--- ljwe/settings.py
+++ ljwe/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/public/static'),
+    os.path.join(BASE_DIR, 'frontend/public'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
