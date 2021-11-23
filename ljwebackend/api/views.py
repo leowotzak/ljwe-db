6262f888b2b5a16ee9a690319b631f3dc436e4dd
Leo Wotzak <leojwotzak@gmail.com>
2021-11-23 12:26:45 -0600

    created new django backend

---
+++ ljwebackend/api/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Symbol
+
+import os
+import logging
+from django.http import HttpResponse
+from django.views.generic import View
+from django.conf import settings
+
+
+class FrontendAppView(View):
+    """
+    Serves the compiled frontend entry point (only works if you have run `yarn
+    build`).
+    """
+    index_file_path = os.path.join('staticfiles', 'index.html')
+
+    def get(self, request):
+        try:
+            with open(self.index_file_path) as f:
+                return HttpResponse(f.read())
+        except FileNotFoundError:
+            logging.exception('Production build of app not found')
+            return HttpResponse(
+                """
+                This URL is only used when you have built the production
+                version of the app. Visit http://localhost:3000/ instead after
+                running `yarn start` on the frontend/ directory
+                """,
+                status=501,
+            )
