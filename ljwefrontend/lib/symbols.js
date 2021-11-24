7476ccd8303b3a33427926c4781c9e31eaac5cab
Leo Wotzak <leojwotzak@gmail.com>
2021-11-24 13:05:37 -0600

    changed endpoint

--- ljwefrontend/lib/symbols.js
+++ ljwefrontend/lib/symbols.js
@@ -1,5 +1,5 @@
 
 export async function getSymbols() {
-    const res = await fetch("https://blooming-journey-16393.herokuapp.com/ljwe/symbol")
+    const res = await fetch("https://floating-headland-47828.herokuapp.com/ljwe/symbol")
     return res.json()
 }
