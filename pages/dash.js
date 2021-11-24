5dc7d9bf7d60ecbd6335d1d7b2c78618d20ed1c6
Leo Wotzak <leojwotzak@gmail.com>
2021-11-24 16:09:49 -0600

    added real options

--- pages/dash.js
+++ pages/dash.js
@@ -1,39 +1,53 @@
-import React from "react"
-import {Container} from "react-bootstrap"
-import {NavBar, SymbolTable} from "../components"
-import {LineChart, Line} from 'recharts';
-import Select from "react-select"
+import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import { NavBar, SymbolTable } from "../components";
+import { LineChart, Line } from "recharts";
+import Select from "react-select";
 
-import {getSymbols} from '../lib/symbols'
+import { getSymbols } from "../lib/symbols";
 
-
-    
 export async function getStaticProps() {
+  const allSymbolData = await getSymbols();
 
-    const allSymbolData = await getSymbols()
+  return {
+    props: { allSymbolData },
+  };
+}
 
-    return {
-        props: { allSymbolData }
-    }
-  }
+export default function Dash({ allSymbolData }) {
+  // allSymbolData.map(({name, ticker}) => console.log(name, ticker))
 
-export default function Dash(allSymbolData) {
-  console.log(allSymbolData)
+  const ph_data = [
+    { name: "page A", uv: 400, pv: 2400, amt: 2400 },
+    { name: "page A", uv: 600, pv: 2400, amt: 2400 },
+    { name: "page A", uv: 1200, pv: 2400, amt: 2400 },
+  ];
 
-    const ph_data = [{name: 'page A', uv: 400, pv: 2400, amt: 2400}, {name: 'page A', uv: 600, pv: 2400, amt: 2400}, {name: 'page A', uv: 1200, pv: 2400, amt: 2400}];
-    const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-      ]
+  const [options, setOptions] = useState([]);
+  const [selection, setSelection] = useState([]);
 
-    return <Container>
-    <NavBar />
-    <LineChart width={400} height={400} data={ph_data}>
-      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+  const handleChange = (e) => {
+    setSelection(e);
+  };
+
+  useEffect(() => {
+    setOptions(
+      Array.from(allSymbolData, ({ symbol_id, name, ticker }) => ({
+        value: symbol_id,
+        label: `(${ticker}) `.concat(name),
+      }))
+    );
+  }, []);
+
+  return (
+    <Container>
+      <NavBar />
+      <LineChart width={400} height={400} data={ph_data}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
       </LineChart>
-    <Select options={options} isMulti />
+      <Select options={options} isMulti onChange={handleChange} />
       first dash
-    {SymbolTable(allSymbolData)}
+      {/* {SymbolTable(allSymbolData)} */}
     </Container>
+  );
 }
