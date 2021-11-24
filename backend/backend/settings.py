507d6d65086a5d0e3bc79c520647decb078bce1c
Leo Wotzak <leojwotzak@gmail.com>
2021-11-24 12:11:49 -0600

    disabled debug, added allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--zj^4cu=ra4v@ws2pmh5pyjxb^t(3^$$szu)*!tjd6fh(9zwn-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".localhost", "floating-headland-47828.herokuapp.com"]
 
 
 # Application definition
