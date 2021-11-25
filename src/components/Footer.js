94df7d16c563d0a279946fe72975daab42418291
Leo Wotzak <leojwotzak@gmail.com>
2021-11-25 10:03:40 -0600

    added placeholder links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,17 @@
-import {Container, Navbar} from 'react-bootstrap'
+import {Container, Row, Col, Navbar} from 'react-bootstrap'
 
 
 export default () => {
     return (
 <Navbar bg="light" expand="lg" fixed="bottom">
   <Container>
+          <Col sm={10} align="left">
       <p>Written with React & Next.js</p>
+      </Col>
+      <Col sm={2} align="right">
+      <a href="https://github.com/">Github</a>&thinsp;
+      <a href="https://www.leojwotzak.com/">Website</a>
+      </Col>
   </Container>
 </Navbar>
     )
