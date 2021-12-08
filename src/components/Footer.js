38727f7745fa107fee0ddee067b3ada4ce223697
Leo Wotzak <leojwotzak@gmail.com>
2021-12-08 11:55:17 -0600

    added link to github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,18 @@
-import {Container, Row, Col, Navbar} from 'react-bootstrap'
-
+import { Container, Row, Col, Navbar } from "react-bootstrap";
 
 export default () => {
-    return (
-<Navbar bg="light" expand="lg" fixed="bottom">
-  <Container>
-          <Col sm={10} align="left">
-      <p>Written with React & Next.js</p>
-      </Col>
-      <Col sm={2} align="right">
-      <a href="https://github.com/">Github</a>&thinsp;
-      <a href="https://www.leojwotzak.com/">Website</a>
-      </Col>
-  </Container>
-</Navbar>
-    )
-}
+  return (
+    <Navbar bg="light" expand="lg" fixed="bottom">
+      <Container>
+        <Col sm={10} align="left">
+          <p>Written with React & Next.js</p>
+        </Col>
+        <Col sm={2} align="right">
+          <a href="https://github.com/">Github</a>
+          <a href="https://www.leojwotzak.com/">Website</a>
+          <a href="https://github.com/leowotzak/ljwe-backend">Github</a>
+        </Col>
+      </Container>
+    </Navbar>
+  );
+};
