559e0fe59e21e3fac76048919b9b988c1044c4db
Leo Wotzak <leojwotzak@gmail.com>
2021-12-08 11:52:16 -0600

    added placeholder images

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,12 @@
 import {Container} from "react-bootstrap";
 import {PageLayout, LoginForm} from '../components'
+import 'holderjs'
 
 export default () => {
 
     return (
         <PageLayout>
+            <img src="holder.js/300x200" />
             <h1>This is the login page</h1>
             <p>If you log in you should be able to access the web tools</p>
             <LoginForm />
