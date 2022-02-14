7f370982346c84912718c2f95ec5d6b6d3340860
Leo Wotzak <69174910+leowotzak@users.noreply.github.com>
2022-02-14 12:18:52 -0600

    Add fontawesome icons dependency

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,17 @@
 import { Container, Row, Col, Navbar } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-solid-svg-icons";
 
 export default () => {
   return (
-    <Navbar bg="light" expand="lg" fixed="bottom">
+    <Navbar bg="light" expand="lg" fixed="bottom" style={{ height: "30px" }}>
       <Container>
-        <Col sm={10} align="left">
-          <p>Written with React & Next.js</p>
-        </Col>
-        <Col sm={2} align="right">
-          <a href="https://github.com/">Github</a>
+        <div>Written with React & Next.js</div>
+        <div>
+          <FontAwesomeIcon icon={["fab", "fa-github"]}>sssss</FontAwesomeIcon>
           <a href="https://www.leojwotzak.com/">Website</a>
           <a href="https://github.com/leowotzak/ljwe-backend">Github</a>
-        </Col>
+        </div>
       </Container>
     </Navbar>
   );
